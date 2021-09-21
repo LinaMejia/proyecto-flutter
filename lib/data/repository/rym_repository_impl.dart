3539c6b9309b3df9e1fc09d93794d1c808a48f26
Michael <michael.stiver.pardo@gmail.com>
2021-09-20 23:01:41 -0500

    add repository

---
+++ lib/data/repository/rym_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:proyecto_flutter/domain/rym_repository.dart';
+
+class RYMRepositoryImpl extends RYMRepository{
+  @override
+  Future<Either<String, String>> getCharacterDetail() async {
+    // TODO: implement getCharacterDetail
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> getCharacterList() async {
+    // TODO: implement getCharacterList
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> getEpisodeList() async {
+    // TODO: implement getEpisodeList
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/domain/rym_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+
+abstract class RYMRepository{
+  Future<Either<String, String>> getCharacterList();
+  Future<Either<String, String>> getEpisodeList();
+  Future<Either<String, String>> getCharacterDetail();
+}
