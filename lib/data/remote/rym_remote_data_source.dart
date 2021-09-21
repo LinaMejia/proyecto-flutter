b02054b76d02f378512e55468891dc062a5a8d21
Michael <michael.stiver.pardo@gmail.com>
2021-09-20 23:06:58 -0500

    add remote

---
+++ lib/data/remote/rym_remote_data_source.dart
@@ -0,0 +1,24 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:proyecto_flutter/data/remote/rym_remote_source.dart';
+
+class RYMRemoteDataSource extends RYMRemoteSource{
+  @override
+  Future<Either<String, String>> getCharacterDetail() {
+    // TODO: implement getCharacterDetail
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> getCharacterList() {
+    // TODO: implement getCharacterList
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<String, String>> getEpisodeList() {
+    // TODO: implement getEpisodeList
+    throw UnimplementedError();
+  }
+
+}
