4ea4c87fd4ae186898b5fcd7f290fa1f2d76401f
AlexanderFuquene <hover.unilanos@hotmail.com>
2021-09-23 18:41:47 -0500

    actualizacion

--- lib/presentation/home/home_provider.dart
+++ lib/presentation/home/home_provider.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_flutter/domain/rym_repository.dart';
+import 'package:proyecto_flutter/domain/model/episode_model.dart';
 
 class HomeProvider extends ChangeNotifier {
-
   final RYMRepository _rymRepository;
   HomeProvider(this._rymRepository);
 
   int _currentPage = 0;
+  List<Result>? _episodes;
+  List<Result>? get episodes => _episodes;
 
-  void setCurrentPage(int index){
+  void setCurrentPage(int index) {
     this._currentPage = index;
     notifyListeners();
   }
 
-  int getCurrentPage(){
+  int getCurrentPage() {
     return _currentPage;
+  }
+
+  void getEpisodes() async {
+    final data = await _rymRepository.getEpisodeList();
+    data.fold((l) => {}, (r) => _episodes = r.results);
+    notifyListeners();
   }
 }
