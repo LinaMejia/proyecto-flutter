94461ca9abbc3f37aa3820f0204756acd18e4dc3
AlexanderFuquene <hover.unilanos@hotmail.com>
2021-09-25 08:27:01 -0500

    avance

--- lib/presentation/episodes/episode_list_page.dart
+++ lib/presentation/episodes/episode_list_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:proyecto_flutter/presentation/home/home_provider.dart';
 
 class EpisodeListPage extends StatelessWidget {
   const EpisodeListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<HomeProvider>(
+        builder: (, snapshot, _) {
+          if (snapshot.episodes == null) {
+            return Center(child: CircularProgressIndicator());
+          }else{
+            final result = snapshot.episodes!;
+            return ListView.builder(
+              itemCount: result.length,
+              itemBuilder: (_, index) {
+                return ListTile(
+                  title: Text(result[index].name),
+                  subtitle: Text(result[index].airDate),
+                  trailing: Text('Characters ${result[index].characters.length}'),
+                );
+              },
+            );
+          }
+        }
+    );
   }
 }
