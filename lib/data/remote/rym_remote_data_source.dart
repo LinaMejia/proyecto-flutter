8dafc678a4e55b0088730bf7e0131d102e968d72
LinaMejia <linalmejia@gmail.com>
2021-09-24 21:43:16 -0500

    añadir provider de episodios

--- lib/data/remote/rym_remote_data_source.dart
+++ lib/data/remote/rym_remote_data_source.dart
@@ -1,9 +1,9 @@
-
 import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
 import 'package:proyecto_flutter/data/remote/rym_remote_source.dart';
 import 'package:proyecto_flutter/domain/domain/episode_model.dart';
 
-class RYMRemoteDataSource extends RYMRemoteSource{
+class RYMRemoteDataSource extends RYMRemoteSource {
   @override
   Future<Either<String, String>> getCharacterDetail() {
     // TODO: implement getCharacterDetail
@@ -17,9 +17,19 @@ class RYMRemoteDataSource extends RYMRemoteSource{
   }
 
   @override
-  Future<Either<String, Episode>> getEpisodeList() {
-    // TODO: implement getEpisodeList
-    throw UnimplementedError();
+  Future<Either<String, Episode>> getEpisodeList() async {
+    var episodePage;
+    try {
+      final url = Uri.parse('https://rickandmortyapi.com/api/episode');
+      final result = await http.get(url);
+      if (result.statusCode == 200) {
+        episodePage = episodeFromJson(result.body);
+        return Right(episodePage);
+      } else {
+        return Left("");
+      }
+    } catch (e) {
+      return Left(e.toString());
+    }
   }
-
 }
