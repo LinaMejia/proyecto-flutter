4f972de31968a47b34497f848e109518dab2f262
AlexanderFuquene <hover.unilanos@hotmail.com>
2021-09-25 08:35:09 -0500

    avance

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 // @dart=2.9
-
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
-
-
 import 'di/injector.dart';
 import 'presentation/home/home_page.dart';
 import 'presentation/home/home_provider.dart';
 
-void main(){
+void main() {
   setup();
-  runApp(
-      MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
